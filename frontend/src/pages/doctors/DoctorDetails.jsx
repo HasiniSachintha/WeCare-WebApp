5385ea1ecccf6b3deb281bf6a887e7520dfe1a4d
Hasini Sachintha <hasinisachintha22@gmail.com>
2024-10-15 20:43:16 +0530

    feat:login,signup,contact us

--- frontend/src/pages/doctors/DoctorDetails.jsx
+++ frontend/src/pages/doctors/DoctorDetails.jsx
@@ -3,14 +3,15 @@ import doctorImg from '../../assets/images/doctor-img02.png'
 import starIcon from '../../assets/images/Star.png'
 import DoctorAbout from './DoctorAbout'
 import Feedback from './Feedback'
+import SidePanel from './SidePanel'
 
 function DoctorDetails() {
 
   const [tab,setTab] = useState('about')
   return (
     <section>
-      <div className='mx-w-[1170px] px-5 md:mx-[250px] sm:mx-[10px]'>
-        <div className='grid md:grid-cols-2 gap-[50px]'>
+      <div className='mx-w-[1170px] px-5 md:mx-[100px] sm:mx-[5px]'>
+        <div className='grid md:grid-cols-3 gap-[50px]'>
           <div className='md:col-span-2'>
             <div className='flex item-center gap-5'>
               <figure className='max-w-[200px] max-h[200px]'>
@@ -18,7 +19,7 @@ function DoctorDetails() {
 
               </figure>
 
-              <div>
+              <div className='mt-[5px]'>
 
                 <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded'>
                   Surgeon
@@ -51,7 +52,10 @@ function DoctorDetails() {
                 tab === "feedback" && <Feedback/>
               }
             </div>
+          </div>
 
+          <div>
+            <SidePanel/>
           </div>
         </div>
       </div>
